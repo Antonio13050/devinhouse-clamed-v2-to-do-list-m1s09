0bf3a4da78125a92240476c4ac0d5f9df06be916
Antonio Carlos Virissimo <antonio13050@gmail.com>
2023-07-11 11:25:05 -0700

    Configurações de deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: "/devinhouse-clamed-v2-to-do-list-m1s09",
+    plugins: [react()],
+});
